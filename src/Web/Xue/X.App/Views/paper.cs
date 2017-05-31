6b615f4f2d7c2717b3db7d7585272e1086e3a348
x <dreamzk@126.com>
2017-05-31 09:06:19 +0800

    fw

---
+++ src/Web/Xue/X.App/Views/paper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+
+namespace X.App.Views
+{
+    public class paper : xview
+    {
+        public int id { get; set; }
+        protected override string GetParmNames
+        {
+            get
+            {
+                return "id";
+            }
+        }
+        protected override void InitDict()
+        {
+            base.InitDict();
+            
+            
+
+
+        }
+    }
+}
