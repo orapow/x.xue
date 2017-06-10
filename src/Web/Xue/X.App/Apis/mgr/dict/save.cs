b85f8dfa98f76a5188f4170d73fffd558a2825ba
x <dreamzk@126.com>
2017-06-10 09:34:39 +0800

    修改细节

--- src/Web/Xue/X.App/Apis/mgr/dict/save.cs
+++ src/Web/Xue/X.App/Apis/mgr/dict/save.cs
@@ -7,30 +7,30 @@ using X.Data;
 using X.Web;
 using X.Web.Com;
 
-namespace X.App.Apis.mgr.dict {
-    public class save : xmg {
-        public int id {get; set;}
+namespace X.App.Apis.mgr.dict
+{
+    public class save : xmg
+    {
+        public int id { get; set; }
         [ParmsAttr(req = true, name = "名称")]
-        public string name {get; set; }
-        public string upv {get; set;}
-        public string val {get; set;}
+        public string name { get; set; }
         public string code { get; set; }
-        protected override XResp Execute() {
+        protected override XResp Execute()
+        {
             x_dict ent = null;
-            if (id > 0)
-                ent = DB.x_dict.FirstOrDefault(o => o.dict_id == id);
-            if (ent == null)
-                ent = new x_dict() { code = code };
+            if (id > 0) ent = DB.x_dict.FirstOrDefault(o => o.dict_id == id);
+            if (ent == null) ent = new x_dict() { code = code };
+
             ent.name = name;
-            ent.value = val;
-            if (ent.id == 0) {
+            ent.upval = "0";
+
+            if (ent.id == 0)
+            {
                 DB.x_dict.InsertOnSubmit(ent);
                 SubmitDBChanges();
             }
-            if (upv == ent.value || string.IsNullOrEmpty(upv))
-                upv = "0";
-            ent.upval = upv;
-            ent.value = val;
+
+            if (string.IsNullOrEmpty(ent.value)) ent.value = ent.id + "";
 
             SubmitDBChanges();
             CacheHelper.Remove("dict." + ent.code);
--- src/Web/Xue/X.App/Views/mgr/dict/edit.cs
+++ src/Web/Xue/X.App/Views/mgr/dict/edit.cs
@@ -1,40 +1,41 @@
 ﻿using System.Linq;
 using X.Web;
 
-namespace X.App.Views.mgr.dict {
-    public class edit : xmg {
-        public int id {
+namespace X.App.Views.mgr.dict
+{
+    public class edit : xmg
+    {
+        public int id
+        {
             get; set;
         }
-        public string pid {
+        public string pid
+        {
             get; set;
         }
-        public string code {
+        public string code
+        {
             get; set;
         }
-        protected override string GetParmNames {
-            get {
+        protected override string GetParmNames
+        {
+            get
+            {
                 return "id-code";
             }
         }
-        protected override void InitDict() {
+        protected override void InitDict()
+        {
             base.InitDict();
 
-            if (id > 0) {
+            if (id > 0)
+            {
                 var ent = DB.x_dict.FirstOrDefault(o => o.dict_id == id);
-                if (ent == null)
-                    throw new XExcep("0x0005");
+                if (ent == null) throw new XExcep("0x0005");
                 dict.Add("item", ent);
-                pid = ent.upval.Split('-').Last();
             }
             var q = DB.x_dict.FirstOrDefault(o => o.code == code);
             dict.Add("coe", q);
-            //var up = GetDict("question.topic", pid);
-            //if (up != null)
-            //    dict.Add("up", pid + "|" + up.name);
-
-          
-
         }
     }
 }
